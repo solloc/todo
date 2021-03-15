72e4bb68b1dbe5580761989ae2cc73b45e60c409
Matthias Maisch <matthias@maisch.me>
2021-03-15 21:45:56 +0100

    function to create default task added

--- tasks/create.js
+++ tasks/create.js
@@ -1,18 +1,51 @@
 'use strict';
 
-module.exports.create = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'task create',
-        input: event,
-      },
-      null,
-      2
-    ),
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+module.exports.create = (event, context, callback) => {
+
+  // const timestamp = new Date().getTime();
+
+  // return {
+  //   statusCode: 200,
+  //   body: JSON.stringify(
+  //     {
+  //       message: 'task create',
+  //       input: event,
+  //     },
+  //     null,
+  //     2
+  //   ),
+  // };
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      TaskID: uuid.v1(),
+      Status: "OPEN",
+      text: "default text"
+    },
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  dynamoDB.put(params, (error, data) => {
+
+    if (error) {
+      console.error(error);
+      const response = {
+        statusCode: 501,
+        body: JSON.stringify(params),
+      };
+      callback(null, response);
+      return;
+    }
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params),
+    };   
+    callback(null, response);
+  });
 };
