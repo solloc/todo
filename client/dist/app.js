8764e610a86c24b5dfe1fe24e031fcf3c282af60
Matthias Maisch <matthias@maisch.me>
2021-03-26 15:11:53 +0100

    basic stylesheets added #15

--- client/dist/app.js
+++ client/dist/app.js
@@ -4,19 +4,29 @@ $(document).ready(function(){
 });
 
 var get_tasks_endpoint = "https://xhjn5xxmn5.execute-api.eu-central-1.amazonaws.com/dev/tasks"
+// get_tasks_endpoint = "../resources/tasks.json"
 
 async function refreshTaskList() {
     console.log("tasks are refreshed");
     const response = await fetch(get_tasks_endpoint);
     const tasks = await response.json();
 
-    // var taskListElement = $("#content").html("<ul></ul>");
-    var taskListElement = $("<ul></ul>");
-    taskListElement.appendTo("#content");
+    var taskListContainer = $("div").addClass("space-y-4");
+    taskListContainer.appendTo("#content");
+
+    var taskList = "";
 
     var i;
     for(i = 0; i < tasks.length; i++) {
         var task = tasks[i];
-        taskListElement.append("<li><b>" + task["taskID"] + "</b>: " + task["text"] + "</li>");
+        taskList += `
+            <div class="bg-white rounded-xl shadow border p-2">
+                <div class="flex">
+                    <div class="m-1 bg-gray-200 hover:bg-gray-300 rounded-full px-2 font-bold text-sm leading-loose cursor-pointer">/` + task["taskID"] + `</div>
+                </div>
+                <div class="px-2">` + task["text"] + `</div>
+            </div>`;
     }
+
+    taskListContainer.html(taskList);
 }
