d16cd1b05bca4174cefde0dbf76c071458af2372
Matthias Maisch <matthias@maisch.me>
2021-03-26 16:39:58 +0100

    spinner added #14

--- client/dist/app.js
+++ client/dist/app.js
@@ -4,29 +4,59 @@ $(document).ready(function(){
 });
 
 var get_tasks_endpoint = "https://xhjn5xxmn5.execute-api.eu-central-1.amazonaws.com/dev/tasks"
-// get_tasks_endpoint = "../resources/tasks.json"
+var delay = 0;
+
+// for test purposes
+// get_tasks_endpoint = "../resources/tasks.json";
+// delay = 3000;
 
 async function refreshTaskList() {
     console.log("tasks are refreshed");
-    const response = await fetch(get_tasks_endpoint);
-    const tasks = await response.json();
 
-    var taskListContainer = $("div").addClass("space-y-4");
-    taskListContainer.appendTo("#content");
+    $.ajax({
+        url: get_tasks_endpoint,
+        beforeSend: addSpinner,
+        dataType: "json",
+    }).done(function(data) {
+        setTimeout(function(){
+            console.log("data received");
+            $(".fa-circle-notch").hide();
+    
+            // const response = await fetch(get_tasks_endpoint);
+            // const tasks = await data.json();
+            const tasks = data;
+    
+            var taskListContainer = $("div").addClass("space-y-4");
+            taskListContainer.appendTo("#content");
+    
+            var taskList = "";
+    
+            var i;
+            for(i = 0; i < tasks.length; i++) {
+                var task = tasks[i];
+                taskList += `
+                    <div class="bg-white rounded-xl shadow border p-2">
+                        <div class="flex">
+                            <div class="m-1 bg-gray-200 hover:bg-gray-300 rounded-full px-2 font-bold text-sm leading-loose cursor-pointer">/` + task["taskID"] + `</div>
+                        </div>
+                        <div class="px-2">` + task["text"] + `</div>
+                    </div>`;
+            }    
+            taskListContainer.html(taskList);              
+        }, delay);
+    });
+}
 
-    var taskList = "";
-
-    var i;
-    for(i = 0; i < tasks.length; i++) {
-        var task = tasks[i];
-        taskList += `
-            <div class="bg-white rounded-xl shadow border p-2">
-                <div class="flex">
-                    <div class="m-1 bg-gray-200 hover:bg-gray-300 rounded-full px-2 font-bold text-sm leading-loose cursor-pointer">/` + task["taskID"] + `</div>
-                </div>
-                <div class="px-2">` + task["text"] + `</div>
-            </div>`;
-    }
-
-    taskListContainer.html(taskList);
+var addSpinner = function() {
+    console.log("adding spinner");
+    
+    var spinner = $("<i>").addClass("fas fa-circle-notch fa-spin");
+    var spinnercontainer = $("<div>").addClass("flex justify-center").append(spinner);
+        // .ajaxStart(function() {
+        //     $( this ).show();
+        // })
+        // .ajaxStop(function() {
+        //     $( this ).hide();
+        // });    
+    $("#content").append(spinnercontainer);
 }
