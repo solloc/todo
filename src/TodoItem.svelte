969da024e51366a904a8994124e14d1438c7fa18
Matthias Maisch <matthias@maisch.me>
2021-04-06 20:10:24 +0200

    updater added (in work)

--- src/TodoItem.svelte
+++ src/TodoItem.svelte
@@ -5,13 +5,14 @@
     // import { externalLink } from 'svelte-awesome/icons';
 
     // // import external-link-alt
+    let taskDetails;
 
     async function getTodoItem() {
         const res = await fetch(`https://xhjn5xxmn5.execute-api.eu-central-1.amazonaws.com/dev/tasks/${params.id}`);
-        const text = await res.json();
+        taskDetails = await res.json();
 
         if (res.ok) {
-            return text;
+            return taskDetails;
         } else {
             throw new Error(text);
         }
@@ -29,6 +30,31 @@
     //     // promise = getTodos();
     //     promise = getTodos();
     // }
+    async function saveData() {
+        console.log(`saving ${JSON.stringify(taskDetails)}`);
+
+        const settings = {
+            method: 'PUT',
+            mode: 'cors',
+            body: JSON.stringify(taskDetails),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+            }
+        }
+
+        // TODO: check cors settings ... everywhere
+
+        const res = await fetch(`https://xhjn5xxmn5.execute-api.eu-central-1.amazonaws.com/dev/tasks/${params.id}`, settings);
+        const resString = await res.json();
+        // taskDetails = await res.json();
+
+        if (res.ok) {
+            // return taskDetails;
+            return resString;
+        } else {
+            throw new Error(resString);
+        }
+    }
 </script>
 
 
@@ -36,10 +62,14 @@
     <b>ID:</b> {params.id}
     {#await promise}
         <p class="text-blue-500 text-center">loading task details</p>
-    {:then task} 
-        <p>{task.text}</p>
-        <p>STATUS: {task.status}</p>
-        <p>Created at: {task.createdAt}</p>
-        <p>Modified at: {task.modifiedAt}</p>
+    {:then} 
+        <form on:submit|preventDefault={saveData}>
+            <textarea bind:value={taskDetails.text} class="w-full"></textarea>
+            <p><i>{taskDetails.text}</i></p>
+            <p>STATUS: {taskDetails.status}</p>
+            <p>Created at: {taskDetails.createdAt}</p>
+            <p>Modified at: {taskDetails.modifiedAt}</p>
+            <button type="submit">save</button>
+        </form>
     {/await}
 </div>
